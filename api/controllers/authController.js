4280a6db9570f0fc02e9b96cb77958501e016ed1
domivbhh <hariharanvbit123@gmail.com>
2024-03-28 12:02:27 +0530

    created mw and function to handle errors

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,7 +1,8 @@
 import User from "../models/UserModel.js"
 import bcryptjs from 'bcryptjs'
+import { errorHandler } from "../utils/error.js"
 
-export const signup=async (req,res)=>{
+export const signup=async (req,res,next)=>{
     const {username,email,password}=req.body
     const hashedPassword=bcryptjs.hashSync(password,10)
     const newUser=new User({username,password:hashedPassword,email})/// idhu dha andha model ku anupudhu
@@ -10,7 +11,8 @@ export const signup=async (req,res)=>{
     await newUser.save()        ///idhu dha db la andha model create panna use aaagudhu
     res.status(201).json('user created successfully')
     }
-    catch(err){
-        res.status(500).send(err.message)
+    catch(error)
+    {
+        next(error)
     }
 }
--- api/index.js
+++ api/index.js
@@ -16,6 +16,8 @@ const app=express()
 
 app.use(express.json())
 
+
+
 app.listen(3000,()=>{
     console.log('server is running at Port 3000..')
 })
@@ -27,3 +29,13 @@ app.listen(3000,()=>{
 
 app.use('/api/user',userRouter)
 app.use('/api/auth',authRouter)
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal server error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
