756ab06dfafbb6a8218db1ab10ee6698d8096f7f
domivbhh <hariharanvbit123@gmail.com>
2024-03-28 20:49:20 +0530

    created sign-in route and logic

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,6 +1,9 @@
 import User from "../models/UserModel.js"
 import bcryptjs from 'bcryptjs'
 // import { errorHandler } from "../utils/error.js"
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const signup=async (req,res,next)=>{
     
@@ -15,4 +18,34 @@ export const signup=async (req,res,next)=>{
     catch (error) {
       next(error);
     }
+}
+
+
+
+export const signin= async (req,res,next)=>{
+  const{email,password}=req.body;
+  try{
+    const validUser=await User.findOne({email})
+    if(!validUser){
+      return next(errorHandler(404,'user not found'))
+    }
+    const validPassword=bcryptjs.compareSync(password,validUser.password)
+    if(!validPassword){
+      return next(errorHandler(401, "password does not match"));
+    }
+
+    const token=jwt.sign({id:validUser._id},process.env.JWT_SECRET)
+
+    // const{password:pass,...rest}=validUser._doc///destructure password and other info(I.E -- in REST)
+
+    const sendingData = await User.findOne({email:email }).select("username");
+    
+    res.cookie('token',token,{httpOnly:true}).status(200).json(sendingData)
+
+
+  }
+  catch(err){
+      next(err);
+
+  }
 }
