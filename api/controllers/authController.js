cb57aa4bf02cd26733546f9866af5043e4d5301e
domivbhh <hariharanvbit123@gmail.com>
2024-03-28 19:51:42 +0530

    created signup page linking Db i.e storing getting data

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,18 +1,18 @@
 import User from "../models/UserModel.js"
 import bcryptjs from 'bcryptjs'
-import { errorHandler } from "../utils/error.js"
+// import { errorHandler } from "../utils/error.js"
 
 export const signup=async (req,res,next)=>{
-    const {username,email,password}=req.body
-    const hashedPassword=bcryptjs.hashSync(password,10)
-    const newUser=new User({username,password:hashedPassword,email})/// idhu dha andha model ku anupudhu
     
-    try{
-    await newUser.save()        ///idhu dha db la andha model create panna use aaagudhu
-    res.status(201).json('user created successfully')
-    }
-    catch(error)
-    {
-        next(error)
+      const { username, email, password } = req.body;
+      const hashedPassword = bcryptjs.hashSync(password, 10);
+      const newUser = new User({ username, password: hashedPassword, email }); /// idhu dha andha model ku anupudhu
+    
+      try {
+      await newUser.save(); ///idhu dha db la andha model create panna use aaagudhu
+      res.status(201).json("user created successfully");
+    } 
+    catch (error) {
+      next(error);
     }
 }
--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Signin = () => {
   return (
     <div>
       signin
+      <br/>
+      <Link to='/sign-up'>Signup</Link>
     </div>
   )
 }
