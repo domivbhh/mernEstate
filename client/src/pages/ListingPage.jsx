fe0135ec9ed4383513df1e8c0d4509ce85eb721b
domivbhh <hariharanvbit123@gmail.com>
2024-04-05 11:08:46 +0530

    created Listing page frontend

--- client/src/pages/ListingPage.jsx
+++ client/src/pages/ListingPage.jsx
@@ -4,6 +4,7 @@ import {Swiper,SwiperSlide} from 'swiper/react';
 import SwiperCore from 'swiper'
 import {Navigation} from 'swiper/modules'
 import 'swiper/css/bundle'
+import { FaBath, FaBed, FaChair, FaMapMarkerAlt, FaParking } from 'react-icons/fa';
 
 
 const ListingPage = () => {
@@ -42,27 +43,93 @@ useEffect(()=>{
               <p className="text-center my-7 text-2xl">Loading......</p>
             )}
             {error && (
-                <div>
-                        <p className="text-center my-7 text-2xl">Error occured......</p>
-                        <Link to='/' className='text-center my-7 text-2xl'>Back to Home</Link>
-                </div>
+              <div>
+                <p className="text-center my-7 text-2xl">Error occured......</p>
+                <Link to="/" className="text-center my-7 text-2xl">
+                  Back to Home
+                </Link>
+              </div>
             )}
 
-            {listingData && !error && !loading &&(
-                    <>
-                    <Swiper navigation>
-                        {listingData.imageUrls.map((item)=>(<SwiperSlide key={item}>
-                            <div className='h-[550px]' style={{background:`url(${item}) center no-repeat` ,backgroundSize:'cover'}}>
+            {listingData && !error && !loading && (
+              <>
+                <Swiper navigation>
+                  {listingData.imageUrls.map((item) => (
+                    <SwiperSlide key={item}>
+                      <div
+                        className="h-[550px]"
+                        style={{
+                          background: `url(${item}) center no-repeat`,
+                          backgroundSize: "cover",
+                        }}
+                      ></div>
+                      <div className="flex flex-col max-w-4xl mx-auto p-3 my-7 gap-6">
+                        <p className="text-2xl font-semibold">
+                          {listingData.name}-${" "}
+                          {listingData.offer
+                            ? listingData.discountedPrice.toLocaleString(
+                                "en-US"
+                              )
+                            : listingData.regularPrice.toLocaleString("en-US")}
+                          {listingData.type === "rent" && " /month "}
+                        </p>
 
-                            </div>
-
-                        </SwiperSlide>))}
-                    </Swiper>
-                    
-                    </>
-
-                )}
-
+                        <p className="flex items-center mt-6 gap-2 text-slate-600 my-2 text-sm">
+                          <FaMapMarkerAlt className="text-green" />
+                          {listingData.address}
+                        </p>
+                        <div className="flex gap-4 ">
+                          <p className="bg-red-900 w-full max-w-[200px] text-white text-center p-1 rounded-md">
+                            {listingData.type === "rent"
+                              ? "For Rent"
+                              : "For Sale"}
+                          </p>
+                          {listingData.offer && (
+                            <p className="bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md">
+                              ${" "}
+                              {+listingData.regularPrice -
+                                +listingData.discountedPrice}
+                            </p>
+                          )}
+                        </div>
+                        <p className="text-slate-800">
+                          <span className="font-semibold text-black">
+                            Description-{" "}
+                          </span>
+                          {listingData.description}
+                        </p>
+                        <ul className=" text-green-900 font-semibold text-sm flex flex-wrap items-center gap-4 sm:gap-4">
+                          <li className="flex items-center gap-1 whitespace-nowrap">
+                            <FaBed className="text-lg" />
+                            {listingData.bedrooms > 1
+                              ? `${listingData.bedrooms} beds`
+                              : `${listingData.bedrooms} bed`}
+                          </li>
+                          <li className="flex items-center gap-1 whitespace-nowrap">
+                            <FaBath className="text-lg" />
+                            {listingData.bathrooms > 1
+                              ? `${listingData.bathrooms} baths`
+                              : `${listingData.bathrooms} bath`}
+                          </li>
+                          <li className="flex items-center gap-1 whitespace-nowrap">
+                            <FaParking className="text-lg" />
+                            {listingData.parking 
+                              ? `Parking spot`
+                              : 'No parking'}
+                          </li>
+                          <li className="flex items-center gap-1 whitespace-nowrap">
+                            <FaChair className="text-lg" />
+                            {listingData.furnished
+                              ? `Furnished`
+                              : `Unfurnished`}
+                          </li>
+                        </ul>
+                      </div>
+                    </SwiperSlide>
+                  ))}
+                </Swiper>
+              </>
+            )}
           </main>
         );
   
